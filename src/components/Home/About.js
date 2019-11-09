7ba7d32acf04d13ae9cac250e0400698101922e9
imvamsi <krrishvamsi7@gmail.com>
2019-11-09 13:49:58 -0500

    added about section :label:

---
+++ src/components/Home/About.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Title from "../Title"
+import styles from "../../css/about.module.css"
+import img from "../../images/defaultBcg.jpeg"
+
+const About = () => {
+  return (
+    <section className={styles.about}>
+      <Title title="about" subtitle="us" />
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>Explore the difference</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <button type="button" className="btn-primary">
+            Read More..
+          </button>
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,23 @@
 import React from "react"
-import {Link} from 'gatsby'
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
-import Banner from '../components/Banner'
+import Banner from "../components/Banner"
+import About from "../components/Home/About"
 
 export default () => (
   <Layout>
     <SimpleHero>
-        <Banner title="Continue Exploring" info="lorem re obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical 
-        literature, discovered the">
-            <Link to ="/tours" className="btn-white">Explore More</Link>
-        </Banner>
-
+      <Banner
+        title="Continue Exploring"
+        info="lorem re obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical 
+        literature, discovered the"
+      >
+        <Link to="/tours" className="btn-white">
+          Explore More
+        </Link>
+      </Banner>
     </SimpleHero>
+    <About />
   </Layout>
 )
-         
